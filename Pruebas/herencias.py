67f94f579e3bfa4189e36c1bdba7bc15cd6569e9
luciasucunza <luciasucunza@est.frba.utn.edu.ar>
2019-04-11 17:34:27 -0300

    11/04

---
+++ Pruebas/herencias.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 11 14:56:00 2019
+
+@author: luciasucunza
+"""
+
+class Humano:
+    
+    
+    def __init__(self, edad = 4, sexo = 'F', nacionalidad = 'a', fecha = '01/01/90', apellido = 'A', nombre = 'N' ):
+        self.edad = edad
+        self.sexo = sexo
+        self.naci = nacionalidad
+        self.fech = fecha
+        self.apel = apellido
+        self.nomb = nombre
+                
+        print('Soy', self.nomb, 'tengo', self.edad, 'años')
+    
+    def hablar(self, mensaje):
+        print (mensaje)
+    
+    def crear(self):
+        return Humano()
+    
+    
+    
+    
+padre = Humano( matriz, pendientes, v/s, {0,1-02, 03. 54}, '1/08/90', 'fsd', 'fd') 
+hijos = padre.crear()
+nieto = hijos.crear()
+
+class PARAMETRO:
+    
+    
+    def __init__(self, dataFR):
+        self.df = dataFR
+                
+    def graficar(self, mensaje):
+        print (mensaje)
+    
+    def mediana(self):
+        matriz = mediana cada tres puntos en ua columna y un t
+        
+        return PARAMETRO( matriz )
+        
+    
+    
+pendiente = PARAMETRO( matriz que devuelve el toolbox de mariano ) 
+mediana = pendiente.mediana()
+
