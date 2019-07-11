23ad67d53f957deb448e7baf1c3ecaedf90abed2
luciasucunza <luciasucunza@est.frba.utn.edu.ar>
2019-07-11 19:23:42 -0300

    Completas_obtParam

--- Pruebas.1/obtencionParam.py
+++ Pruebas.1/obtencionParam.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 #%%
 #------Apertura de la Señal-------
-mat_struct = sio.loadmat('git_proyecto_isquemia/TP4_ecg.mat')
+mat_struct = sio.loadmat('/home/luciasucunza/git_proyecto_isquemia/TP4_ecg.mat')
 
 ecg_one_lead    = mat_struct['ecg_lead'].flatten()
 qrs_detections  = mat_struct['qrs_detections'].flatten()
@@ -51,7 +51,7 @@ def plotHistograma( param, intervalo):
     
 #
 #
-def plotParamECG( dt, dx, tipo, fs, ecg):
+def plotParamECG( df, tipo, fs, ecg):
     """
         Plotea un valor del parametro en el ECG, dependiendo de la naturaleza del mismo se realizan ciertas graficas adicionales
         Esta funcion formarìa parte de la clase pero solo la podrìa usar el padre? Porque solo la usa la primera instancia creada
@@ -71,28 +71,20 @@ def plotParamECG( dt, dx, tipo, fs, ecg):
         Notes
         -----
     """   
+    dt = df['Tiempo']
+    dx = df['Param']
     if tipo ==          "intervaloRR":
         
-        vent_inf_m = int((dt-1.2)*fs)
-        vent_sup_m = int((dt+0.6)*fs) 
-        
-        zoom_region = np.arange( vent_inf_m, vent_sup_m, 1  )
-        
-        t_qrs1_m = int(dt*fs)                                                      #El +1 de dx esta porque si dx es impar me quedarìa un nunmero menos de vector, asì me queda de la misma longitud que fs*dx y las pares no me cambia
-        t_qrs0_m = int((dt-dx)*fs)
-        
-
-        referencia = min( ecg[t_qrs0_m] , ecg[t_qrs1_m] )                       #Solo para que la recta RR me quede a un nivel que toque los dos
+        #Defino la ventana que se va a graficar
+        zoom_region = np.arange( int((dt-1.2)*fs), int((dt+0.6)*fs), 1  )
         
         
-        t_rectas     = np.arange( t_qrs0_m, t_qrs1_m, 1 ) /fs 
-        rectas       = np.zeros( int(dx*fs) )                                         #Grafico la recta sede la mitad de los QRS hasta cada QRS
-
-        for i in np.arange(0, int(dx*fs), 1):
-            rectas[i]  = referencia
-
+        referencia = min( ecg[int(dt*fs)] , ecg[int((dt-dx)*fs)] )              #Solo para que la recta RR me quede a un nivel que toque los dos
+        t_rectas     = np.arange( dt-dx, dt, 1/fs ) 
+        rectas       = np.full( int(dx*fs), referencia )                                 
         puntos       = np.matrix([ [dt,referencia] , [dt-dx,referencia] ])           
-            
+                
+        #Ploteo
         plt.figure('Intervalo RR')
         plt.cla()
         plt.plot( zoom_region/fs,   ecg[zoom_region]                                                )
@@ -107,20 +99,20 @@ def plotParamECG( dt, dx, tipo, fs, ecg):
     
     elif tipo ==        "PendienteMax":
         
-        vent_inf_m = int((dt-0.4)*fs)
-        vent_sup_m = int((dt+0.6)*fs)
-        
-        zoom_region = np.arange( vent_inf_m, vent_sup_m, 1)
-        
+        #Defino la ventana que se va a graficar y la longitud de la recta        
+        zoom_region = np.arange( int((dt-0.4)*fs), int((dt+0.6)*fs), 1)    
         len_rec     = 0.01
-        t_rectas    = np.arange( dt-len_rec, dt+len_rec, 1/fs)                  #Grafica la recta de la pendiente maxima a +-20ms de que ocurra
+        
+        #Recta con valor de pendiente maxima que pasa por el punto 
+        t_rectas    = np.arange( dt-len_rec, dt+len_rec, 1/fs)
         rectas      = np.zeros( len(t_rectas) ) 
-        
         for i in np.arange(0, len(t_rectas), 1):
-          rectas[i]    = dx*(i-len(t_rectas)//2) + ( ecg[int(dt*fs)] -dt*dx  )
+          rectas[i]    = dx*fs*(t_rectas[i])*fs + ( ecg[int(dt*fs)] -dt*fs*dx*fs )
         
-        puntos      = ( dt, ecg[int(dt*fs)] )                                                #Grafica el punto de la pendiente maxima             
+        #Punto de la pendiente maxima 
+        puntos      = ( dt, ecg[int(dt*fs)] )                                                            
             
+        #Ploteo
         plt.figure('Pendiente Máxima')
         plt.cla()
         plt.plot( zoom_region/fs,   ecg[zoom_region]                                                 )
@@ -132,43 +124,67 @@ def plotParamECG( dt, dx, tipo, fs, ecg):
         plt.show()  
         
     else:       #     "angulo"
-         
-        vent_inf_m = int((dt-0.4)*fs)
-        vent_sup_m = int((dt+0.4)*fs)
         
-        zoom_region = np.arange( vent_inf_m, vent_sup_m, 1)
+        dt1 = df['t_m1']
+        dt2 = df['t_m2']
+        m1  = df['m1']
+        m2  = df['m2'] 
+        
+        #Defino la ventana que se va a graficar  
+        zoom_region = np.arange( int((dt1-0.4)*fs), int((dt2+0.4)*fs), 1)   
                 
-        t_rectas    = np.arange( (dt-0.025)*fs, (dt+0.025)*fs, 1) /fs           #Grafica la recta de la pendiente maxima positiva
-        rectas      = np.zeros( len(t_rectas) )     
-        for i in np.arange(0, len(t_rectas), 1):
-          rectas[i]    = dx*i + ( ecg[int(dt*fs)] -dt*dx  )
-          
-        t_rectas    = np.arange( (dt-0.025)*fs, (dt+0.025)*fs, 1) /fs           #Grafica la recta de la pendiente maxima negativa
-        rectas      = np.zeros( len(t_rectas) ) 
-        for i in np.arange(0, len(t_rectas), 1):
-          rectas[i]    = -dx*i + ( ecg[int(dt*fs)] +dt*dx  )
-       
+        
+        #Recta con valor de pendiente maxima 
+        t_rectas1    = np.arange( dt1-0.005, dt1+0.018,  1/fs)
+        rectas1      = np.zeros( len(t_rectas1) ) 
+        for i in np.arange(0, len(t_rectas1), 1):
+          rectas1[i]    = m1*fs*(t_rectas1[i])*fs + ( ecg[int(dt1*fs)] -dt1*fs*m1*fs )
+        
+        #Recta con valor de pendiente minima
+        t_rectas2    = np.arange( dt2-0.018,  dt2+0.005, 1/fs)    
+        rectas2      = np.zeros( len(t_rectas2) ) 
+        for i in np.arange(0, len(t_rectas2), 1):
+          rectas2[i]    = m2*fs*(t_rectas2[i])*fs + ( ecg[int(dt2*fs)] -dt2*fs*m2*fs )
+        
+        
+        punto1      = ( dt1, ecg[int(dt1*fs)] )    
+        punto2      = ( dt2, ecg[int(dt2*fs)] )    
+        #Ploteo
+        plt.figure('Pendiente Máxima y Minima que forman el angulo')
+        plt.cla()
+        plt.plot( zoom_region/fs,   ecg[zoom_region]                                                            )
+        plt.plot( t_rectas1,         rectas1,                 label=('Pendiente Positiva: '   +str(m1)      )   )
+        plt.plot( t_rectas2,         rectas2,                 label=('Pendiente Negativa: '   +str(m2)      )   )
+        plt.plot( punto1[0],        punto1[1],    'ro',       label=('Ocurrencia: '           +str(np.round(dt,2))+'ms' )   )
+        plt.plot( punto2[0],        punto2[1],    'ro',       label=('Angulo: '               +str(np.round(dx,2))+'rad')   )
+        plt.xlabel('time (s)')        
+        plt.grid()
+        plt.legend()
+        plt.show()  
         
 
 
 
 #%%
 #------Obtención de Parametros-------
-def intervaloRR ( qrs ):
+def intervaloRR ( qrs, fs = 1 ):
     """
         Obtiene los intervalos RR, devolviendo una matriz con el vector de tiempos y el de intevalos
         El tiempo es el tiempo del segundo QRS (para el primer latido el intervalo es 0)
         (tqrs0 = 0seg, tqrs2 = 25seg        => iRR0 = 25seg, tiRR0 = 25seg )
         
         Para el primer latido copia el RR del siguiente
+        
+        Si la frecuencia de muestreo es 1 quiere decir que o está muestreda
     """
     len_qrs = len(qrs)
+    qrs = qrs/fs
     result = np.array([     qrs,    np.hstack([  qrs[1]-qrs[0], qrs[1:len_qrs]-qrs[0:len_qrs-1]  ])    ])
 
     return result.transpose()
 
 
-def pendienteMax ( ecg, qrs ):
+def pendienteMax ( ecg, qrs, fs = 1 ):
     """
         Obtiene los puntos de maxima pendiente a partir de la ubicación del complejo QRS con una ventana de +/-70 muestras
         Devuelve una matriz con el vector de tiempos en el que ocurre la maxima derivada y el valor de dicha derivada 
@@ -179,77 +195,109 @@ def pendienteMax ( ecg, qrs ):
         si me afecta el poscionamiento temporal del máximo (sin correjir se emciemtra una muestra antes del real)
                 
     """
+    qrs = qrs/fs
     
     result  = np.zeros( (len(qrs), 2) )
     i = 0
     
     for ii in qrs:
         
-        ventDerivada    =   np.diff( ecg[ ii-70 : ii+70 ] )
-        ventDerivada    =   np.hstack([ ventDerivada[0], ventDerivada ])           
-        result[i,:]     =   [ np.argmax(ventDerivada)+ii-70 , ventDerivada.max() ]           
+        ventDerivada    =   np.diff( ecg[ int((ii-0.07)*fs) : int((ii+0.07)*fs) ] )
+        ventDerivada    =   np.hstack([ ventDerivada[0], ventDerivada ])     /fs
+        result[i,:]     =   [ np.argmax(ventDerivada)/fs+ii-0.07 , ventDerivada.max() ]           
         i = i+1    
     
     return result
 
 
 
-def angulo ( ecg, qrs):
+def angulo ( ecg, qrs, fs = 1):
     """
         Obtiene el angulo formado por la pendiente maxima y la minima de cada complejo QRS
         
         angulo = arctg( (m2-m1) / (1+m1*m2) )
         
-       Verificar:  __main__:1: RuntimeWarning: overflow encountered in short_scalars
-                   Si devuelve el angulo correcto o el suplementario 
+        Si: Y1 = m1 * t + (ecg[t1]-t1*m1)
+            Y2 = m2 * t + (ecg[t2]-t2*m2)
+            Pinterseccion = ( (ecg[t1]-t1*m1)-(ecg[t2]-t2*m2)  ;  (ecg[t2]-t2*m2)*m1 - (ecg[t1]-t1*m1)*m2 )
+       
+        En la primera columna devuelve el timpo de intersección
+        En la segunda columna devuleve el valor del angulo (si es positivo hacia arriba si es - hacia abajo)
+        En la tercera columna devuleve el valor el tiempo de ocurrencia de la pendiente maxima
+        En la cuarta  columna devuleve el valor de la pendiente maxima
+        En la quinta  columna devuleve el valor el tiempo de ocurrencia de la pendiente minima
+        En la sexta   columna devuleve el valor de la pendiente minima
+        
+        
+        Otra opcion serìa sacar la 4,5 y 6 y reemplazar en la tercera por 
+#        result[i,2] = (a*d-b*c) / (a-b)
+        Es la poscion en el eje Y de la interecciòn (donde graficar el angulo)
+                      
     """
+    qrs = qrs/fs
     
-    result  = np.zeros( (len(qrs), 2) )
+    
+    result  = np.zeros( (len(qrs), 6) )
     i = 0
     
     for ii in qrs:
         
-        ventDerivada    =   np.diff( ecg[ ii-70 : ii+70 ] )
-        ventDerivada    =   np.hstack([ ventDerivada[0], ventDerivada ])           
-        result[i,0]     =   np.argmin(ventDerivada) - np.argmax(ventDerivada) + ii
-        result[i,1]     =   np.arctan( ( ventDerivada.min()- ventDerivada.max())/(1+ ventDerivada.min()* ventDerivada.max()) )
+        ventDerivada    =   np.diff( ecg[ int((ii-0.07)*fs) : int((ii+0.07)*fs) ] )
+        ventDerivada    =   np.hstack([ ventDerivada[0], ventDerivada ])     /fs
+
+        a = ventDerivada.max()
+        c = ecg[np.argmax(ventDerivada)] - np.argmax(ventDerivada) * a
+        b = ventDerivada.min()
+        d = ecg[np.argmin(ventDerivada)] - np.argmin(ventDerivada) * b
+        
+        result[i,0] = (d-c)     / (a-b) + ii
+        result[i,1] = np.arctan( np.abs(a-b) / (1+a*b) )
+        
+        result[i,2] = np.argmax(ventDerivada)/fs + ii-0.07
+        result[i,3] = a
+        result[i,4] = np.argmin(ventDerivada)/fs + ii-0.07
+        result[i,5] = b
+                     
         i = i+1   
     
+    return result
+
 #%%
 #------ PRUEBA DE FUNCIONES: INTERVALO RR------
 fs = 1000
 
-RR = intervaloRR(qrs_detections)
-df_param =  {    'Tiempo'   : RR[:,0] / fs,
-                 'Param'    : RR[:,1] / fs
+RR = intervaloRR(qrs_detections,fs)
+df_param =  {    'Tiempo'   : RR[:,0],
+                 'Param'    : RR[:,1]
                  }
 df_param = pd.DataFrame( df_param )
 
-plotParamECG( df_param.iloc[2]['Tiempo'], df_param.iloc[2]['Param'],"intervaloRR", fs, ecg_one_lead )    
+plotParamECG( df_param.iloc[2],"intervaloRR", fs, ecg_one_lead )  
 
 #%%
 #------ PRUEBA DE FUNCIONES: PENDIENTE MAXIMA------
 fs = 1000
 
-PM = pendienteMax(ecg_one_lead, qrs_detections )
-df_param =  {    'Tiempo'   : PM[:,0] / fs,
+PM = pendienteMax(ecg_one_lead, qrs_detections,fs )
+df_param =  {    'Tiempo'   : PM[:,0],
                  'Param'    : PM[:,1] 
                  }
 df_param = pd.DataFrame( df_param )
 
-plotParamECG( df_param.iloc[2]['Tiempo'], df_param.iloc[2]['Param'],"PendienteMax", fs, ecg_one_lead )    
+plotParamECG( df_param.iloc[2],"PendienteMax", fs, ecg_one_lead )    
 
 #%%
 #------ PRUEBA DE FUNCIONES: AGULO------
 
+ANG = angulo(ecg_one_lead, qrs_detections, fs )
+df_param =  {    'Tiempo'   : ANG[:,0],
+                 'Param'    : ANG[:,1], 
+                 't_m1'     : ANG[:,2], 
+                 'm1'       : ANG[:,3], 
+                 't_m2'     : ANG[:,4], 
+                 'm2'       : ANG[:,5]
+                 }
+df_param = pd.DataFrame( df_param )
 
-#%%
-#------ MAS PRUEBAS PARA LAS FUNCIONES DE PLOTEO------
-dt =df_param.iloc[2]['Tiempo']
-dx =df_param.iloc[2]['Param']
-tipo= "PendienteMax"
-fs = 1000
-ecg= ecg_one_lead
-qrs = qrs_detections
-
-    
+plotParamECG( df_param.iloc[2],"Angulo", fs, ecg_one_lead )    
+   
